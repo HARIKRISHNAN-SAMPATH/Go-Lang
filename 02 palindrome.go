691d39f4f63a9dba5ae8e58c003fec8993e798a1
HARIKRISHNAN-SAMPATH <152170863+HARIKRISHNAN-SAMPATH@users.noreply.github.com>
2023-11-28 16:43:03 +0530

    palindrome
    leetcode

---
+++ 02 palindrome.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func palindrome() {
+	var x int = 121
+	var y int = 0
+	z := x
+
+	for x > 0 {
+
+	}
+	fmt.Println("palindrome", y == z)
+
+}
+func main() {
+
+	palindrome()
+
+}
